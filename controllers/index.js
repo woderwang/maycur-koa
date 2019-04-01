ea65882744692d1fff72de80ecb93d42c7869720
rongwei <rongwei_alisa@hotmail.com>
2019-04-01 11:49:57 +0800

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
-var fn_index = async (ctx, next) => {
-  console.log('hhh');
-  ctx.response.body = `
+const fn_index = async (ctx, next) => {
+    console.log('hhh');
+    ctx.response.body = `
     <h1>Index</h1>
     <form action="/signin" method="post">
       <p>Name: <input name="name" value="koa" /></p>
@@ -10,23 +10,23 @@ var fn_index = async (ctx, next) => {
   `;
 };
 
-var fn_signin = async (ctx, next) => {
-  console.log(ctx);
-  var name = ctx.request.body.name || '';
-      password = ctx.request.body.password || '';
-  console.log(`signin with name: ${name}, password: ${password}`);
- 
-  if(name === 'koa' && password === '12345') {
-    ctx.response.body = `<h1>Welcome, ${name}</h1>`;
-  } else {
-    ctx.response.body = `
+const fn_signin = async (ctx, next) => {
+    console.log(ctx);
+    const name = ctx.request.body.name || '';
+    password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}</h1>`;
+    } else {
+        ctx.response.body = `
       <h1>Login failed</h1>
       <p><a href="/">Try again</a></p>
     `;
-  }
+    }
 };
 
 module.exports = {
-  'GET /': fn_index,
-  'POST /signin': fn_signin
+    'GET /': fn_index,
+    'POST /signin': fn_signin
 };
