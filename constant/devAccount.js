7a5af9a31932dccc2b5e4129dcd3589f25111088
lianjiewang <13325914779@163.com>
2019-04-04 13:44:39 +0800

    account

--- constant/devAccount.js
+++ constant/devAccount.js
@@ -1,6 +1,667 @@
-let account = {
-   env:'dev',
-   account:'111@maycur.com',
-   password:'123456'
-}
+let account = [{
+	"env": "dev",
+	"account": "mc1903041j582m0w@dingtalk.com",
+	"name": "黄渊",
+	"searchKey": "黄渊hy"
+}, {
+	"env": "dev",
+	"account": "mc18092511huh0qo@dingtalk.com",
+	"name": "余旋",
+	"searchKey": "余旋yx"
+}, {
+	"env": "dev",
+	"account": "3801511129593629@dingtalk.com",
+	"name": "王连杰",
+	"searchKey": "王连杰wlj"
+}, {
+	"env": "dev",
+	"account": "mc1803221nogsg00@dingtalk.com",
+	"name": "孟文",
+	"searchKey": "孟文mw"
+}, {
+	"env": "dev",
+	"account": "mc1803221nohd14w@dingtalk.com",
+	"name": "陈恩明",
+	"searchKey": "陈恩明cem"
+}, {
+	"env": "dev",
+	"account": "mc1803221nohwe0w@dingtalk.com",
+	"name": "姚宇帆1",
+	"searchKey": "姚宇帆1yyf1"
+}, {
+	"env": "dev",
+	"account": "mc1803221noifnr4@dingtalk.com",
+	"name": "卫蓉",
+	"searchKey": "卫蓉wr"
+}, {
+	"env": "dev",
+	"account": "mc1803221noiw934@dingtalk.com",
+	"name": "向阳-小白",
+	"searchKey": "向阳-小白xy-xb"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhhf2m8@dingtalk.com",
+	"name": "孙勤",
+	"searchKey": "孙勤sq"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhis6ww@dingtalk.com",
+	"name": "郑培煜",
+	"searchKey": "郑培煜zpy"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhkdce8@dingtalk.com",
+	"name": "周耿",
+	"searchKey": "周耿zg"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhkpa80@dingtalk.com",
+	"name": "小孟",
+	"searchKey": "小孟xm"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhl0ykg@dingtalk.com",
+	"name": "罗敏",
+	"searchKey": "罗敏lm"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhlcglc@dingtalk.com",
+	"name": "史涛",
+	"searchKey": "史涛st"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhlo83k@dingtalk.com",
+	"name": "钱文菁",
+	"searchKey": "钱文菁qwq"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhm0k5c@dingtalk.com",
+	"name": "johnny",
+	"searchKey": "johnnyjohnny"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhmchz4@dingtalk.com",
+	"name": "王森元",
+	"searchKey": "王森元wsy"
+}, {
+	"env": "dev",
+	"account": "mc1803221vhi2tj4@dingtalk.com",
+	"name": "Beata",
+	"searchKey": "beatabeata"
+}, {
+	"env": "dev",
+	"account": "mc1803221nwnxd6o@dingtalk.com",
+	"name": "王晓阳",
+	"searchKey": "王晓阳wxy"
+}, {
+	"env": "dev",
+	"account": "mc1803261zuge03k@dingtalk.com",
+	"name": "王崛超",
+	"searchKey": "王崛超wjc"
+}, {
+	"env": "dev",
+	"account": "mc1803281u9qaups@dingtalk.com",
+	"name": "老吴",
+	"searchKey": "老吴lw"
+}, {
+	"env": "dev",
+	"account": "mc1804021y0yi680@dingtalk.com",
+	"name": "大武",
+	"searchKey": "大武dw"
+}, {
+	"env": "dev",
+	"account": "mc1804111g5qhczk@dingtalk.com",
+	"name": "樊英（跪求pd，java开发）",
+	"searchKey": "樊英（跪求pd，java开发）fy"
+}, {
+	"env": "dev",
+	"account": "mc180420ynggd1c@dingtalk.com",
+	"name": "唐超",
+	"searchKey": "唐超tc"
+}, {
+	"env": "dev",
+	"account": "mc1805021oow8cn4@dingtalk.com",
+	"name": "季盈光",
+	"searchKey": "季盈光jyg"
+}, {
+	"env": "dev",
+	"account": "mc1805021q6hc16o@dingtalk.com",
+	"name": "阮腊梅",
+	"searchKey": "阮腊梅rlm"
+}, {
+	"env": "dev",
+	"account": "mc18050313sftwqo@dingtalk.com",
+	"name": "金敏",
+	"searchKey": "金敏jm"
+}, {
+	"env": "dev",
+	"account": "mc1805081hv0y9s0@dingtalk.com",
+	"name": "测试张王威",
+	"searchKey": "测试张王威cszww"
+}, {
+	"env": "dev",
+	"account": "mc1805081p1ysgsg@dingtalk.com",
+	"name": "向轩瑾",
+	"searchKey": "向轩瑾xyx"
+}, {
+	"env": "dev",
+	"account": "mc1805151mlwnf28@dingtalk.com",
+	"name": "zpy",
+	"searchKey": "zpyzpy"
+}, {
+	"env": "dev",
+	"account": "mc1805151mlxepz4@dingtalk.com",
+	"name": "陈海兰",
+	"searchKey": "陈海兰chl"
+}, {
+	"env": "dev",
+	"account": "mc1805151mlxjabk@dingtalk.com",
+	"name": "史涛1",
+	"searchKey": "史涛1st1"
+}, {
+	"env": "dev",
+	"account": "mc1805151mlyw9vk@dingtalk.com",
+	"name": "查永青",
+	"searchKey": "查永青cyq"
+}, {
+	"env": "dev",
+	"account": "mc1805151mlz7dog@dingtalk.com",
+	"name": "陈鹏",
+	"searchKey": "陈鹏cp"
+}, {
+	"env": "dev",
+	"account": "mc1805161dkm8yrk@dingtalk.com",
+	"name": "小萌",
+	"searchKey": "小萌xm"
+}, {
+	"env": "dev",
+	"account": "mc18061422od0pvk@dingtalk.com",
+	"name": "胡善琪",
+	"searchKey": "胡善琪hsq"
+}, {
+	"env": "dev",
+	"account": "mc18061422onuwow@dingtalk.com",
+	"name": "林萍",
+	"searchKey": "林萍lp"
+}, {
+	"env": "dev",
+	"account": "mc18061422oo9yps@dingtalk.com",
+	"name": "张1",
+	"searchKey": "张1z1"
+}, {
+	"env": "dev",
+	"account": "mc18061422openls@dingtalk.com",
+	"name": "张3",
+	"searchKey": "张3z3"
+}, {
+	"env": "dev",
+	"account": "mc180621zavn1ts@dingtalk.com",
+	"name": "方烈",
+	"searchKey": "方烈fl"
+}, {
+	"env": "dev",
+	"account": "mc180621zavtmv4@dingtalk.com",
+	"name": "李西川",
+	"searchKey": "李西川lxc"
+}, {
+	"env": "dev",
+	"account": "mc180621zaxxr0g@dingtalk.com",
+	"name": "崔春美",
+	"searchKey": "崔春美ccm"
+}, {
+	"env": "dev",
+	"account": "mc180629147herk0@dingtalk.com",
+	"name": "李明华",
+	"searchKey": "李明华lmh"
+}, {
+	"env": "dev",
+	"account": "mc180711x2uyv40@dingtalk.com",
+	"name": "李祥",
+	"searchKey": "李祥lx"
+}, {
+	"env": "dev",
+	"account": "mc1807111b1sopog@dingtalk.com",
+	"name": "顾文婷",
+	"searchKey": "顾文婷gwt"
+}, {
+	"env": "dev",
+	"account": "mc1807171cj0nta8@dingtalk.com",
+	"name": "陈秀秀",
+	"searchKey": "陈秀秀cxx"
+}, {
+	"env": "dev",
+	"account": "mc1808091f2u4u80@dingtalk.com",
+	"name": "刘艺",
+	"searchKey": "刘艺ly"
+}, {
+	"env": "dev",
+	"account": "mc1808131cncoz5s@dingtalk.com",
+	"name": "孙翊轩",
+	"searchKey": "孙翊轩syx"
+}, {
+	"env": "dev",
+	"account": "mc1808131cnd3lds@dingtalk.com",
+	"name": "陈冬",
+	"searchKey": "陈冬cd"
+}, {
+	"env": "dev",
+	"account": "mc180815xgh9kao@dingtalk.com",
+	"name": "石朋珍",
+	"searchKey": "石朋珍spz"
+}, {
+	"env": "dev",
+	"account": "mc180815xia0s1s@dingtalk.com",
+	"name": "朱敏强",
+	"searchKey": "朱敏强zmq"
+}, {
+	"env": "dev",
+	"account": "mc180821xbquh34@dingtalk.com",
+	"name": "陈宇亮",
+	"searchKey": "陈宇亮cyl"
+}, {
+	"env": "dev",
+	"account": "mc1808291cgzo9a8@dingtalk.com",
+	"name": "叶正卿",
+	"searchKey": "叶正卿yzq"
+}, {
+	"env": "dev",
+	"account": "mc1809111gkg5hj4@dingtalk.com",
+	"name": "果果",
+	"searchKey": "果果gg"
+}, {
+	"env": "dev",
+	"account": "mc18091315gr9c00@dingtalk.com",
+	"name": "曾凡鑫",
+	"searchKey": "曾凡鑫zfx"
+}, {
+	"env": "dev",
+	"account": "mc18091716e0ensw@dingtalk.com",
+	"name": "项子晨",
+	"searchKey": "项子晨xzc"
+}, {
+	"env": "dev",
+	"account": "mc18092613xsx9ts@dingtalk.com",
+	"name": "邱蒙蒙",
+	"searchKey": "邱蒙蒙qmm"
+}, {
+	"env": "dev",
+	"account": "mc1810121etm6fwg@dingtalk.com",
+	"name": "风尘",
+	"searchKey": "风尘fc"
+}, {
+	"env": "dev",
+	"account": "mc18101313hz7rwg@dingtalk.com",
+	"name": "罗成",
+	"searchKey": "罗成lc"
+}, {
+	"env": "dev",
+	"account": "mc1810151261pw5c@dingtalk.com",
+	"name": "孙修我",
+	"searchKey": "孙修我sxw"
+}, {
+	"env": "dev",
+	"account": "mc1810151262ayo0@dingtalk.com",
+	"name": "何士届",
+	"searchKey": "何士届hsj"
+}, {
+	"env": "dev",
+	"account": "mc1810161dyeu1a8@dingtalk.com",
+	"name": "陈东文",
+	"searchKey": "陈东文cdw"
+}, {
+	"env": "dev",
+	"account": "mc1810291et6ksn4@dingtalk.com",
+	"name": "胡秋悦",
+	"searchKey": "胡秋悦hqy"
+}, {
+	"env": "dev",
+	"account": "mc181030118h3ta8@dingtalk.com",
+	"name": "小石",
+	"searchKey": "小石xs"
+}, {
+	"env": "dev",
+	"account": "mc1810311fkq639c@dingtalk.com",
+	"name": "辛晶艺",
+	"searchKey": "辛晶艺xjy"
+}, {
+	"env": "dev",
+	"account": "mc1810311fm07q4g@dingtalk.com",
+	"name": "林强强",
+	"searchKey": "林强强lqq"
+}, {
+	"env": "dev",
+	"account": "mc1810311fmem4u8@dingtalk.com",
+	"name": "郑全武",
+	"searchKey": "郑全武zqw"
+}, {
+	"env": "dev",
+	"account": "mc1812121d1ct0jk@dingtalk.com",
+	"name": "陆健卿",
+	"searchKey": "陆健卿ljq"
+}, {
+	"env": "uat",
+	"account": "mc1903041i98yx34@dingtalk.com",
+	"name": "黄渊",
+	"searchKey": "黄渊hy"
+}, {
+	"env": "uat",
+	"account": "mc180925y8ql24g@dingtalk.com",
+	"name": "余旋",
+	"searchKey": "余旋yx"
+}, {
+	"env": "uat",
+	"account": "mc1804111g5rfa4g@dingtalk.com",
+	"name": "樊英（跪求pd，java开发）",
+	"searchKey": "樊英（跪求pd，java开发）fy"
+}, {
+	"env": "uat",
+	"account": "mc180408209uxqtc@dingtalk.com",
+	"name": "王连杰",
+	"searchKey": "王连杰wlj"
+}, {
+	"env": "uat",
+	"account": "mc180408209vftvk@dingtalk.com",
+	"name": "孟文",
+	"searchKey": "孟文mw"
+}, {
+	"env": "uat",
+	"account": "mc180408209vs16o@dingtalk.com",
+	"name": "陈恩明",
+	"searchKey": "陈恩明cem"
+}, {
+	"env": "uat",
+	"account": "mc180408209w17gg@dingtalk.com",
+	"name": "姚宇帆1",
+	"searchKey": "姚宇帆1yyf1"
+}, {
+	"env": "uat",
+	"account": "mc180408209wbtvk@dingtalk.com",
+	"name": "卫蓉",
+	"searchKey": "卫蓉wr"
+}, {
+	"env": "uat",
+	"account": "mc180408209wlhj4@dingtalk.com",
+	"name": "大武",
+	"searchKey": "大武dw"
+}, {
+	"env": "uat",
+	"account": "mc180408209wvhts@dingtalk.com",
+	"name": "向阳-小白",
+	"searchKey": "向阳-小白xy-xb"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2xlbsw@dingtalk.com",
+	"name": "王晓阳",
+	"searchKey": "王晓阳wxy"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2xqlfk@dingtalk.com",
+	"name": "孙勤",
+	"searchKey": "孙勤sq"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2xzlds@dingtalk.com",
+	"name": "Beata",
+	"searchKey": "beatabeata"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2yz0n4@dingtalk.com",
+	"name": "周耿",
+	"searchKey": "周耿zg"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2z6qrk@dingtalk.com",
+	"name": "王崛超",
+	"searchKey": "王崛超wjc"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2zaww0@dingtalk.com",
+	"name": "老吴",
+	"searchKey": "老吴lw"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2zf30g@dingtalk.com",
+	"name": "罗敏",
+	"searchKey": "罗敏lm"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2zmi2o@dingtalk.com",
+	"name": "钱文菁",
+	"searchKey": "钱文菁qwq"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2zq22o@dingtalk.com",
+	"name": "johnny",
+	"searchKey": "johnnyjohnny"
+}, {
+	"env": "uat",
+	"account": "mc1804101c2ztvk0@dingtalk.com",
+	"name": "王森元",
+	"searchKey": "王森元wsy"
+}, {
+	"env": "uat",
+	"account": "mc180420yihroxs@dingtalk.com",
+	"name": "唐超",
+	"searchKey": "唐超tc"
+}, {
+	"env": "uat",
+	"account": "mc1805021nt6eark@dingtalk.com",
+	"name": "季盈光",
+	"searchKey": "季盈光jyg"
+}, {
+	"env": "uat",
+	"account": "mc1805021q6ihmv4@dingtalk.com",
+	"name": "阮腊梅",
+	"searchKey": "阮腊梅rlm"
+}, {
+	"env": "uat",
+	"account": "mc18050313sh181s@dingtalk.com",
+	"name": "金敏",
+	"searchKey": "金敏jm"
+}, {
+	"env": "uat",
+	"account": "mc1805081hv23nk0@dingtalk.com",
+	"name": "测试张王威",
+	"searchKey": "测试张王威cszww"
+}, {
+	"env": "uat",
+	"account": "mc1805081p1zy2gw@dingtalk.com",
+	"name": "向轩瑾",
+	"searchKey": "向轩瑾xyx"
+}, {
+	"env": "uat",
+	"account": "mc18051512iv7wn4@dingtalk.com",
+	"name": "zpy",
+	"searchKey": "zpyzpy"
+}, {
+	"env": "uat",
+	"account": "mc1805151c4z3g8w@dingtalk.com",
+	"name": "陈海兰",
+	"searchKey": "陈海兰chl"
+}, {
+	"env": "uat",
+	"account": "mc1805151csp9qm8@dingtalk.com",
+	"name": "史涛1",
+	"searchKey": "史涛1st1"
+}, {
+	"env": "uat",
+	"account": "mc1805151d2a6j28@dingtalk.com",
+	"name": "查永青",
+	"searchKey": "查永青cyq"
+}, {
+	"env": "uat",
+	"account": "mc1805151dck45c0@dingtalk.com",
+	"name": "陈鹏",
+	"searchKey": "陈鹏cp"
+}, {
+	"env": "uat",
+	"account": "mc180516zcthb7k@dingtalk.com",
+	"name": "小萌",
+	"searchKey": "小萌xm"
+}, {
+	"env": "uat",
+	"account": "mc1805301cv5pji8@dingtalk.com",
+	"name": "崔春美",
+	"searchKey": "崔春美ccm"
+}, {
+	"env": "uat",
+	"account": "mc1805312wd55hc@dingtalk.com",
+	"name": "方烈",
+	"searchKey": "方烈fl"
+}, {
+	"env": "uat",
+	"account": "mc18060126drtdds@dingtalk.com",
+	"name": "李西川",
+	"searchKey": "李西川lxc"
+}, {
+	"env": "uat",
+	"account": "mc1806131e512ozk@dingtalk.com",
+	"name": "林萍",
+	"searchKey": "林萍lp"
+}, {
+	"env": "uat",
+	"account": "mc1806141enkgrnk@dingtalk.com",
+	"name": "胡善琪",
+	"searchKey": "胡善琪hsq"
+}, {
+	"env": "uat",
+	"account": "mc1806141x36t1q8@dingtalk.com",
+	"name": "张3",
+	"searchKey": "张3z3"
+}, {
+	"env": "uat",
+	"account": "mc1806141x6nnhmo@dingtalk.com",
+	"name": "张1",
+	"searchKey": "张1z1"
+}, {
+	"env": "uat",
+	"account": "mc180629142rxlvk@dingtalk.com",
+	"name": "李明华",
+	"searchKey": "李明华lmh"
+}, {
+	"env": "uat",
+	"account": "mc180711x2vt5hc@dingtalk.com",
+	"name": "李祥",
+	"searchKey": "李祥lx"
+}, {
+	"env": "uat",
+	"account": "mc1807111b1t0s8w@dingtalk.com",
+	"name": "顾文婷",
+	"searchKey": "顾文婷gwt"
+}, {
+	"env": "uat",
+	"account": "mc1807171cj0pe68@dingtalk.com",
+	"name": "陈秀秀",
+	"searchKey": "陈秀秀cxx"
+}, {
+	"env": "uat",
+	"account": "mc180813yunfzeo@dingtalk.com",
+	"name": "孙翊轩",
+	"searchKey": "孙翊轩syx"
+}, {
+	"env": "uat",
+	"account": "mc180813yyo1mgw@dingtalk.com",
+	"name": "陈冬",
+	"searchKey": "陈冬cd"
+}, {
+	"env": "uat",
+	"account": "mc180815xggyi2o@dingtalk.com",
+	"name": "石朋珍",
+	"searchKey": "石朋珍spz"
+}, {
+	"env": "uat",
+	"account": "mc180815xiaavi8@dingtalk.com",
+	"name": "朱敏强",
+	"searchKey": "朱敏强zmq"
+}, {
+	"env": "uat",
+	"account": "mc180821xbqo8ow@dingtalk.com",
+	"name": "陈宇亮",
+	"searchKey": "陈宇亮cyl"
+}, {
+	"env": "uat",
+	"account": "mc1808291cgyt81s@dingtalk.com",
+	"name": "叶正卿",
+	"searchKey": "叶正卿yzq"
+}, {
+	"env": "uat",
+	"account": "mc1809101m917e2o@dingtalk.com",
+	"name": "刘艺",
+	"searchKey": "刘艺ly"
+}, {
+	"env": "uat",
+	"account": "mc1809111e5iq680@dingtalk.com",
+	"name": "果果",
+	"searchKey": "果果gg"
+}, {
+	"env": "uat",
+	"account": "mc18091314bpzm68@dingtalk.com",
+	"name": "曾凡鑫",
+	"searchKey": "曾凡鑫zfx"
+}, {
+	"env": "uat",
+	"account": "mc180917102kdzb4@dingtalk.com",
+	"name": "项子晨",
+	"searchKey": "项子晨xzc"
+}, {
+	"env": "uat",
+	"account": "mc180926zt5wtts@dingtalk.com",
+	"name": "邱蒙蒙",
+	"searchKey": "邱蒙蒙qmm"
+}, {
+	"env": "uat",
+	"account": "mc1810111ik5yneo@dingtalk.com",
+	"name": "风尘",
+	"searchKey": "风尘fc"
+}, {
+	"env": "uat",
+	"account": "mc18101313dzkmww@dingtalk.com",
+	"name": "罗成",
+	"searchKey": "罗成lc"
+}, {
+	"env": "uat",
+	"account": "mc1810131jutc1kw@dingtalk.com",
+	"name": "孙修我",
+	"searchKey": "孙修我sxw"
+}, {
+	"env": "uat",
+	"account": "mc181015125ebsow@dingtalk.com",
+	"name": "何士届",
+	"searchKey": "何士届hsj"
+}, {
+	"env": "uat",
+	"account": "mc1810161dv0px4w@dingtalk.com",
+	"name": "陈东文",
+	"searchKey": "陈东文cdw"
+}, {
+	"env": "uat",
+	"account": "mc1810291et9hhj4@dingtalk.com",
+	"name": "胡秋悦",
+	"searchKey": "胡秋悦hqy"
+}, {
+	"env": "uat",
+	"account": "mc181030118j7cw0@dingtalk.com",
+	"name": "小石",
+	"searchKey": "小石xs"
+}, {
+	"env": "uat",
+	"account": "mc1810311fks0xz4@dingtalk.com",
+	"name": "辛晶艺",
+	"searchKey": "辛晶艺xjy"
+}, {
+	"env": "uat",
+	"account": "mc1810311fly4g00@dingtalk.com",
+	"name": "林强强",
+	"searchKey": "林强强lqq"
+}, {
+	"env": "uat",
+	"account": "mc1810311fmga1vk@dingtalk.com",
+	"name": "郑全武",
+	"searchKey": "郑全武zqw"
+}, {
+	"env": "uat",
+	"account": "mc1812111nlfi0w0@dingtalk.com",
+	"name": "陆健卿",
+	"searchKey": "陆健卿ljq"
+}];
 module.exports = account;
