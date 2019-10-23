81975960e68f981c77426231375b5e8fcfb93390
huangyuan <maycur-tech@Maycur-Tech.local>
2019-10-23 14:33:02 +0800

    增加插入数据方法

--- api/maycurDevAccount.js
+++ api/maycurDevAccount.js
@@ -11,14 +11,13 @@ router.get('/dev-account/:env?', (ctx, next) => {
     })
 });
 
-/* 插入数据用的 暂时注释掉
 router.post('/account-insert', (ctx, next) => {
-    return Sql.insertData({ tableName: 'dev_account', values: ctx.request.body.data }).then(res => {
+    return Sql.insertData({ tableName: 'dev_account', values: JSON.parse(ctx.request.body) }).then(res => {
         ctx.body = '数据插入成功'
     }, err => {
         throw Error(err);
     })
 });
-*/
+
 
 module.exports = router;
